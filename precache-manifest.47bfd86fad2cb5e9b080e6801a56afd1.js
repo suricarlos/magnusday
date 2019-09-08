1e695f8ae4abe9243864adf2889055f68d55cb87
Carlos Bruno <carlao.linux@gmail.com>
2019-09-08 11:35:53 -0300

    Updates

---
+++ precache-manifest.47bfd86fad2cb5e9b080e6801a56afd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88892871a69c76c755b8ebb86cfb1e1",
+    "url": "/magnusday/index.html"
+  },
+  {
+    "revision": "0728b7ee8d698114a1e6",
+    "url": "/magnusday/static/css/main.5b6c86cd.chunk.css"
+  },
+  {
+    "revision": "2f41c8e6870c79fbb858",
+    "url": "/magnusday/static/js/2.826a952d.chunk.js"
+  },
+  {
+    "revision": "0728b7ee8d698114a1e6",
+    "url": "/magnusday/static/js/main.1a3cdb08.chunk.js"
+  },
+  {
+    "revision": "5eeee3963fa6803abb1a",
+    "url": "/magnusday/static/js/runtime~main.13572680.js"
+  },
+  {
+    "revision": "327c021af8cb22f3b0da523ef9707cd6",
+    "url": "/magnusday/static/media/beep.327c021a.mp3"
+  },
+  {
+    "revision": "ceb8c47fc126f2cb9bf46b7b7a282fff",
+    "url": "/magnusday/static/media/cronometro.ceb8c47f.svg"
+  }
+]);
